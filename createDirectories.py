614fb2e10e7218a86468d80d1106f04c7d59bd26
Albert Szostkiewicz <tmdag@tmdag.com>
2018-09-03 09:21:30 -0700

    renamed classes for readibility and added test method

--- createDirectories.py
+++ createDirectories.py
@@ -6,8 +6,8 @@ Create directory tree template with
 import pathlib
 import os
 
-class ExtractStructure:
-    """ Class that creates list of directories from json file """
+class _ExtractFromJson:
+    """ Internal, hidden class to extract directories from Json File """
     def __init__(self, directory):
         """ initialize global variables """
         self.dir_structure = ""
@@ -35,7 +35,7 @@ class ExtractStructure:
         self.dir_structure = sorted(self.dir_structure[:-1].split(","))
         return self.dir_structure
 
-class Structure:
+class SetupTree:
     """ Main working class to re-create folder structure """
     def __init__(self, initial_dir, template_file="folderTemplate.json"):
         """ check if file exists, load json file and extract directories from it """
@@ -43,16 +43,26 @@ class Structure:
         if not os.path.isfile(template_file):
             raise Exception('file {0} not found\n'.format(template_file))
         self.template = JsonFile(template_file).load()
-        self.structure = ExtractStructure(self.template[0]).aslist()
+        self.structure = _ExtractFromJson(self.template[0]).aslist()
 
-    def create(self, project_name):
+    def test_structure(self, project_name):
+        """ Create directory for every line in a list of directories """
+        for single_path in self.structure:
+            s_path = self.init_dir + project_name + single_path[1:]
+            print("Directory to be created: {}".format(s_path))
+
+    def create_new_project(self, project_name):
         """ Create directory for every line in a list of directories """
         for single_path in self.structure:
             s_path = self.init_dir + project_name + single_path[1:]
             # https://stackoverflow.com/questions/273192/how-can-i-safely-create-a-nested-directory-in-python
             pathlib.Path(s_path).mkdir(parents=True, exist_ok=True)
-            print(s_path)
+            print("Creating Directory: {}".format(s_path))
 
 if __name__ == '__main__':
     from jsonParser.jsonParser import JsonFile
-    Structure("/home/Projects/", template_file="folderTemplate.json").create("Project002")
+    projects_parent_dir = "/home/Projects/"
+    dir_structure = SetupTree(projects_parent_dir, template_file="folderTemplate.json")
+    new_project_name = "Project002"
+    dir_structure.test_structure(new_project_name)
+    dir_structure.create_new_project(new_project_name)
