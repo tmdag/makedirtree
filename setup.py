e999b37bf2611c6c9bf468ba21784334252af0d1
Albert Szostkiewicz <tmdag@tmdag.com>
2019-05-25 15:44:20 -0700

    prepearing it as a module

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from setuptools import setup
 setup(name='makedirtree',
       version='0.1',
       description='helper for making directory structures',
-      url='http://tmserver:3000/tmdag/makedirtree',
+      url='https://github.com/tmdag/makedirtree',
       author='Albert Szostkiewicz',
       author_email='tmdag@tmdag.com',
       license='MIT',
-      packages=['makedirtree'],
+      packages=['makedirtree','pathlib'],
+      dependency_links=['http://github.com/tmdag/jsonParser'],
+      install_requires=['jsonparser',],
       zip_safe=False)
